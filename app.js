786afd27785acba56ee1e8e5fc23296d8411561d
Hirusha-Kavinda <hirusha631@gmail.com>
2024-09-05 15:05:46 +0530

    implement chunk and buffer

--- app.js
+++ app.js
@@ -14,9 +14,15 @@
 
 
 const http = require('http');
+const fs = require('fs');
+const { log } = require('console');
+
+
+
 const server = http.createServer((req, res) => {
 
      const url = req.url;
+     const method = req.method;
      if(url === '/'){
           res.setHeader('Content-Type', 'text/html');
           res.write('<html>')
@@ -28,6 +34,28 @@ const server = http.createServer((req, res) => {
 
     // console.log(req.url , req.method, req.headers);
      //process.exit();
+
+     if (url === '/message' && method === 'POST') {
+          const body = [];
+          req.on('data', (chunk) => {
+               console.log(chunk)
+               body.push(chunk);
+              // console.log(body);
+          });
+
+          req.on('end' , () =>{
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+           // console.log(" massage eka pako :"+ message);
+            fs.writeFileSync('message.txt', message);
+
+          })
+
+         
+          res.statusCode = 302;
+          res.setHeader('Location' , '/')
+          return res.end();
+     }
   
      res.setHeader('Content-Type', 'text/html');
      res.write('<html>')
